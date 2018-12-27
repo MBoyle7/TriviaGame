4258a0437574539f8826b836eb41d7c3e690d45c
Matthew Boyle <rockchalkmatt7@gmail.com>
2018-12-26 23:40:45 -0600

    TriviaGame

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,3 +1,19 @@
+function timer(){
+    var sec = 30;
+    var timer = setInterval(function(){
+        document.getElementById('timer').innerHTML='00:'+sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(timer);
+            alert("You ran out of time!");
+        }
+    }, 1000);
+}
+
+window.onload = function() {
+    timer();
+}
+
 function submitAnswers() {
     var total = 4;
     var score = 0;
@@ -9,33 +25,45 @@ function submitAnswers() {
 
     if(q1 == null || q1 == '') {
         alert('You missed question 1');
-         
+        return false;  
     };
     
     if(q2 == null || q2 == '') {
         alert('You missed question 2');
-         
+        return false;  
     };
 
     if(q3 == null || q3 == '') {
         alert('You missed question 3');
-        
+        return false;  
     };
 
     if(q4 == null || q4 == '') {
         alert('You missed question 4');
-         
+        return false;  
     };
 
     var answers = ["b","c","a","d"];
 
-    for(i = 1, i <= total, i++) {
-        if(eval('q'+i) == answers[i-1]) {
-            score++
-        }
+    if(q1 == answers[0]) {
+        score++;
     };
 
+    if(q2 == answers[1]) {
+        score++;
+    };
+
+    if(q3 == answers[2]) {
+        score++;
+    };
+
+    if(q4 == answers[3]) {
+        score++;
+    };
     
+    var results = document.getElementById('results');
+    results.innerHTML = '<h3>You scored <span>'+score+'</span> out of <span>'+total+'</span></h3>';
+	alert('You scored '+score+' out of ' +total);
 }    
 
 
